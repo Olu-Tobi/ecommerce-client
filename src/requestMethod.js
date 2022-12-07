e8ba692550080138f0b75dcb2b9b6f627f94473a
unknown <tobidaramola77@gmail.com>
2022-12-07 20:19:45 +0100

    fifth commit

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecommerce-app-api.onrender.com/api/"
+const BASE_URL = "http://localhost:5000/api/"
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzZiODE1NjlhNzdmZjU0YTQ0MzFhMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2NDUzMDU2OSwiZXhwIjoxNjY0NzAzMzY5fQ.nIqu64tm5jxAACU-PKesEOTHF56WwiG-T9XX4dhPS_I'
 
 
